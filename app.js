8f0ec6790f5d69ffff755ff8bc2cceba9e5b8c73
rizwan <118995669+riz-33@users.noreply.github.com>
2024-06-16 01:37:20 +0500

    update

---
+++ app.js
@@ -0,0 +1,44 @@
+var min = 0;
+var sec = 0;
+var msec = 0;
+
+var timer = false;
+
+function play() {
+    timer = true;
+    myFunc();
+}
+
+function reset(){
+    location.reload();
+}
+
+function myFunc() {
+    if (timer == true) {
+        msec = msec + 1;
+        if (msec == 100) {
+            sec = sec + 1;
+            msec = 0;
+        }
+        if (sec == 60) {
+            min = min + 1
+            sec = 0;
+        }
+
+        var getSec = sec;
+        var getMin = min;
+
+        if (sec < 10){
+            getSec = "0"+sec;
+        }
+        if (min < 10){
+            getMin = "0"+min;
+        }
+
+        setTimeout ("myFunc()",10)
+
+        document.getElementById ("msec").innerHTML= msec;
+        document.getElementById ("sec").innerHTML=getSec;
+        document.getElementById ("min").innerHTML=getMin;
+    }
+}
